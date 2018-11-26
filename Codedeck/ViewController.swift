5239ad6b14eb721f78d9b03b0974c60ce8a44da7
James Sherlock <James.Sherlock@sky.uk>
2018-11-26 13:34:46 +0000

    Initial Commit

---
+++ Codedeck/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Codedeck
+//
+//  Created by Sherlock, James on 26/11/2018.
+//  Copyright © 2018 Sherlouk. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ CodedeckTests/CodedeckTests.swift
@@ -0,0 +1,34 @@
+//
+//  CodedeckTests.swift
+//  CodedeckTests
+//
+//  Created by Sherlock, James on 26/11/2018.
+//  Copyright © 2018 Sherlouk. All rights reserved.
+//
+
+import XCTest
+@testable import Codedeck
+
+class CodedeckTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
