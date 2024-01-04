a021dac0c249c4a3328560df5b763259dfe3cf3b
James Sherlock <15193942+Sherlouk@users.noreply.github.com>
2024-01-04 21:48:53 +0000

    Added setImage from Grant Butler

--- Sources/Codedeck/StreamDeckKey.swift
+++ Sources/Codedeck/StreamDeckKey.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CoreGraphics
 
 public class StreamDeckKey {
     
@@ -92,6 +93,63 @@ public class StreamDeckKey {
             }
         }
         
+    }
+    
+    public func setImage(withActions actions: (CGContext, CGSize) -> Void) {
+        let contextSize = CGSize(width: streamDeck.product.iconSize, height: streamDeck.product.iconSize)
+
+        guard let context = CGContext(
+            data: nil,
+            width: Int(contextSize.width), 
+            height: Int(contextSize.height),
+            bitsPerComponent: 8,
+            bytesPerRow: 0,
+            space: CGColorSpaceCreateDeviceRGB(),
+            bitmapInfo: CGImageAlphaInfo.noneSkipLast.rawValue
+        ) else {
+            Logger.error("Could not create a context for StreamDeck")
+            return
+        }
+
+        context.setFillColor(.black)
+        context.fill(CGRect(origin: .zero, size: contextSize))
+
+        actions(context, contextSize)
+
+        guard let rawPointer = context.data else {
+            Logger.error("Could not get data for the context")
+            return
+        }
+
+        let numberOfBytes = context.height * context.bytesPerRow
+
+        let buffer = UnsafeBufferPointer(start: rawPointer.bindMemory(to: UInt8.self, capacity: numberOfBytes), count: numberOfBytes)
+        let imageData = Data(buffer: buffer)
+
+        switch streamDeck.product {
+        case .streamDeck, .streamDeckMini:
+            let firstPageImageData = imageData[0 ..< StreamDeckKey.NUM_FIRST_PAGE_PIXELS]
+            let secondPageImageData = imageData[StreamDeckKey.NUM_FIRST_PAGE_PIXELS ..< StreamDeckKey.NUM_FIRST_PAGE_PIXELS + StreamDeckKey.NUM_SECOND_PAGE_PIXELS]
+
+            let firstPage = streamDeck.dataPageOne(keyIndex: keyIndex, data: firstPageImageData)
+            let secondPage = streamDeck.dataPageTwo(keyIndex: keyIndex, bufIndex: 0, data: secondPageImageData)
+
+            streamDeck.write(data: firstPage)
+            streamDeck.write(data: secondPage)
+            
+        case .streamDeckXL:
+            let packetSize = streamDeck.product.pagePacketSize - 8 // header size
+            
+            guard let pages = imageData.processJPEG(width: streamDeck.product.iconSize)?.chunked(into: packetSize) else {
+                Logger.error("Failed to process JPEG from image data")
+                return
+            }
+            
+            for (index, page) in pages.enumerated() {
+                let pageDetails = streamDeck.dataPageTwo(keyIndex: keyIndex, bufIndex: index, data: Data(page), isLast: index == pages.count - 1)
+                streamDeck.write(data: pageDetails)
+            }
+        }
     }
     
     // Private
